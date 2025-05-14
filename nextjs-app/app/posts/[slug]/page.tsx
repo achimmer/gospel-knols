e1337cda2e4af82045278460df6dd5789fae3e62
Andrew Himmer <achimmer@gmail.com>
2025-05-14 11:56:57 -0500

    Post template updates -- removed extra divs

--- nextjs-app/app/posts/[slug]/page.tsx
+++ nextjs-app/app/posts/[slug]/page.tsx
@@ -72,37 +72,34 @@ export default async function PostPage(props: Props) {
 
   return (
     <>
-      <div className="">
-        <div className="container my-12 lg:my-24 grid gap-12">
-          <div>
-            <div className="pb-6 grid gap-6 mb-6 border-b border-gray-100">
-              <div className="max-w-3xl flex flex-col gap-6">
-                <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-7xl">
-                  {post.title}
-                </h2>
-              </div>
-              <div className="max-w-3xl flex gap-4 items-center">
-                {post.author &&
-                  post.author.firstName &&
-                  post.author.lastName && (
-                    <Avatar person={post.author} date={post.date} />
-                  )}
-              </div>
-            </div>
-            <article className="gap-6 grid max-w-4xl">
-              <div className="">
-                <CoverImage image={post.coverImage} priority />
-              </div>
-              {post.content?.length && (
-                <PortableText
-                  className="max-w-2xl"
-                  value={post.content as PortableTextBlock[]}
-                />
+      <div className="container my-12 lg:my-24 grid gap-12">
+        <div className="pb-6 grid gap-6 mb-6 border-b border-gray-100">
+          <div className="max-w-3xl flex flex-col gap-6">
+            <h1 className="text-4xl font-bold tracking-tight text-slate-800 sm:text-5xl lg:text-7xl">
+              {post.title}
+            </h1>
+          </div>
+          <div className="max-w-3xl flex gap-4 items-center">
+            {post.author &&
+              post.author.firstName &&
+              post.author.lastName && (
+                <Avatar person={post.author} date={post.date} />
               )}
-            </article>
           </div>
         </div>
+        <article className="gap-6 grid max-w-4xl justify-center">
+          <div className="w-full">
+            <CoverImage image={post.coverImage} priority />
+          </div>
+          {post.content?.length && (
+            <PortableText
+              className="max-w-2xl"
+              value={post.content as PortableTextBlock[]}
+            />
+          )}
+        </article>
       </div>
+
       <div className="border-t border-gray-100">
         <div className="container my-12 lg:my-24 grid gap-12">
           <aside>
